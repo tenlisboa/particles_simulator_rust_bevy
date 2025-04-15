36af6b90a7391f6036030e1a655fea388a88631a
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-04-14 23:14:50 -0300

    feat: create floor and first particle

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use bevy::{DefaultPlugins, app::App};
 
 #[derive(States, Debug, Eq, PartialEq, Hash, Clone, Default)]
 pub enum AppStates {
-    Run,
     #[default]
+    Run,
     Menu,
 }
 
@@ -17,13 +17,55 @@ fn main() {
         .run();
 }
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera3d::default());
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0., 14., 24.).looking_at(Vec3::new(0., 0., 0.), Vec3::Y),
+    ));
 }
 
 mod game {
-    use bevy::ecs::system::Commands;
+    use bevy::{color::palettes::css::SILVER, prelude::*};
 
-    pub fn setup_game(mut commands: Commands) {
-        // TODO;
+    #[derive(Component)]
+    struct Particle;
+
+    #[derive(Component)]
+    struct Floor;
+
+    pub fn setup_game(
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<StandardMaterial>>,
+    ) {
+        let particle = meshes.add(Sphere::default().mesh().uv(32, 18));
+        let particle_material = materials.add(StandardMaterial {
+            base_color: Color::srgb(1., 0., 0.),
+            ..Default::default()
+        });
+
+        commands.spawn((
+            Floor,
+            Mesh3d(meshes.add(Plane3d::default().mesh().size(50., 50.).subdivisions(10))),
+            MeshMaterial3d(materials.add(Color::from(SILVER))),
+            Transform::from_xyz(0., 0., 0.),
+        ));
+
+        commands.spawn((
+            Particle,
+            Mesh3d(particle),
+            MeshMaterial3d(particle_material),
+            Transform::from_xyz(0., 2., 0.),
+        ));
+
+        commands.spawn((
+            PointLight {
+                shadows_enabled: true,
+                intensity: 10_000_000.,
+                range: 100.,
+                shadow_depth_bias: 0.2,
+                ..Default::default()
+            },
+            Transform::from_xyz(8., 16., 8.),
+        ));
     }
 }

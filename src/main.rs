fb43503e8c5422f2cfe8d824a8da07f34fb3c454
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-04-17 21:07:25 -0300

    feat: start collision among particles

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,12 @@ fn main() {
         .add_systems(OnEnter(AppStates::Run), game::setup_game)
         .add_systems(
             Update,
-            (game::apply_gravity, game::check_for_collision).run_if(in_state(AppStates::Run)),
+            (
+                game::apply_gravity,
+                game::check_for_collision_with_floor,
+                game::check_for_collision_with_particles,
+            )
+                .run_if(in_state(AppStates::Run)),
         )
         .run();
 }
@@ -28,7 +33,7 @@ fn setup(mut commands: Commands) {
 }
 
 mod game {
-    use bevy::{color::palettes::css::SILVER, math::Direction3d, prelude::*};
+    use bevy::{color::palettes::css::SILVER, prelude::*};
 
     #[derive(Component)]
     pub struct Particle;
@@ -39,7 +44,8 @@ mod game {
     #[derive(Component, Deref, DerefMut)]
     pub struct Velocity(Vec3);
 
-    const PARTICLE: Sphere = Sphere::new(0.5);
+    const RADIUS: f32 = 0.5;
+    const PARTICLE: Sphere = Sphere::new(RADIUS);
     const FLOOR_HEIGHT: f32 = 1.0;
 
     pub fn setup_game(
@@ -62,9 +68,17 @@ mod game {
 
         commands.spawn((
             Particle,
-            Mesh3d(particle),
-            MeshMaterial3d(particle_material),
+            Mesh3d(particle.clone()),
+            MeshMaterial3d(particle_material.clone()),
             Transform::from_xyz(0., 16., 0.),
+            Velocity(Vec3::new(0.3, 0., 0.1)),
+        ));
+
+        commands.spawn((
+            Particle,
+            Mesh3d(particle.clone()),
+            MeshMaterial3d(particle_material.clone()),
+            Transform::from_xyz(0., 12., 0.),
             Velocity(Vec3::new(0.3, 0., 0.1)),
         ));
 
@@ -94,7 +108,7 @@ mod game {
         }
     }
 
-    pub fn check_for_collision(
+    pub fn check_for_collision_with_floor(
         mut particles: Query<(&mut Transform, &mut Velocity), With<Particle>>,
     ) {
         for (mut particle_transform, mut particle_velocity) in &mut particles {
@@ -108,5 +122,48 @@ mod game {
             particle_transform.translation.y = floor_offset;
             particle_velocity.0.y = -particle_velocity.0.y * BOUNCE_FACTOR;
         }
+    }
+
+    pub fn check_for_collision_with_particles(
+        mut query: Query<(&mut Transform, &mut Velocity), With<Particle>>,
+    ) {
+        let mut combinations = query.iter_combinations_mut();
+
+        while let Some([(transform_a, mut vel_a), (transform_b, mut vel_b)]) =
+            combinations.fetch_next()
+        {
+            let pos_a = transform_a.translation;
+            let pos_b = transform_b.translation;
+
+            if check_collision(pos_a, RADIUS, pos_b, RADIUS) {
+                resolve_collision(pos_a, &mut vel_a, pos_b, &mut vel_b);
+            }
+        }
+    }
+
+    fn check_collision(pos_a: Vec3, radius_a: f32, pos_b: Vec3, radius_b: f32) -> bool {
+        let distance = pos_a.distance(pos_b);
+        distance < (radius_a + radius_b)
+    }
+
+    fn resolve_collision(pos_a: Vec3, vel_a: &mut Vec3, pos_b: Vec3, vel_b: &mut Vec3) {
+        let collision_normal = (pos_b - pos_a).normalize();
+
+        let relative_velocity = *vel_a - *vel_b;
+        let velocity_along_normal = relative_velocity.dot(collision_normal);
+
+        if velocity_along_normal > 0.0 {
+            println!("Afastando: {}", velocity_along_normal);
+            return;
+        }
+
+        let impulse_scalar = -(1.0 + BOUNCE_FACTOR) * velocity_along_normal / 2.0;
+
+        let impulse = collision_normal * impulse_scalar;
+
+        *vel_a += impulse;
+        *vel_b -= impulse;
+
+        println!("vel_a: {}, vel_b: {}", vel_a, vel_b);
     }
 }
